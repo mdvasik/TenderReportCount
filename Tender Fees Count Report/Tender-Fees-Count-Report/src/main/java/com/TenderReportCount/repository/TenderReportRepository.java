0da683fad3c89744716b25a39f5ede014238ac34
Vasik Shaikh <147395532+mdvasik@users.noreply.github.com>
2024-02-22 19:29:29 +0530

    Update TenderReportRepository.java
    final commit done 
    22/04/24

--- Tender Fees Count Report/Tender-Fees-Count-Report/src/main/java/com/TenderReportCount/repository/TenderReportRepository.java
+++ Tender Fees Count Report/Tender-Fees-Count-Report/src/main/java/com/TenderReportCount/repository/TenderReportRepository.java
@@ -13,52 +13,60 @@ import com.TenderReportCount.entity.TenderReportCount;
 @Repository
 public class TenderReportRepository {
 
-    private JdbcTemplate jdbcTemplate;
+	private JdbcTemplate jdbcTemplate;
 
-    public TenderReportRepository(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-   
+	public TenderReportRepository(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
 
-    public List<TenderReportCount> findBySearchingKeyByDate(String searchKey) {
-        String sql = "SELECT * FROM TenderReport_Count_Wise";
-        return jdbcTemplate.query(sql, new TenderReportCountRowMapper());
-    }
-    
-    private static class TenderReportCountRowMapper implements RowMapper<TenderReportCount> {
-    	
-        @Override
-        public TenderReportCount mapRow(ResultSet rs, int rowNum) throws SQLException {
-            TenderReportCount tenderReportCount = new TenderReportCount();
-            
-            // Map columns of the ResultSet to fields of the TenderReportCount object
-            tenderReportCount.setSrNo(rs.getFloat("Sr_No"));
-            tenderReportCount.setTenderId(rs.getFloat("tenderid"));
-            tenderReportCount.setTenderNo(rs.getString("tenderNo"));
-            tenderReportCount.setTenderBrief(rs.getString("tenderBrief"));
-            tenderReportCount.setTotalEMDPaid(rs.getFloat("TotalEMDPaid"));
-            tenderReportCount.setTotalDocFees(rs.getFloat("TotalDocFees"));
-            tenderReportCount.setTotalEventwiseReg(rs.getFloat("TotalEventwiseReg"));
-            tenderReportCount.setTotalFinalSubmission(rs.getFloat("TotalFinalSubmission"));
-            tenderReportCount.setDomainName(rs.getString("domainName"));
-            tenderReportCount.setClientName(rs.getString("clientName"));
-            tenderReportCount.setStartDate(rs.getDate("StartDate").toLocalDate());
-            tenderReportCount.setEndDate(rs.getDate("EndDate").toLocalDate());
-            tenderReportCount.setPublishDate(rs.getDate("PublishDate").toLocalDate());
-            tenderReportCount.setOrganization(rs.getString("Organization"));
-            tenderReportCount.setDepartment(rs.getString("Department"));  
-            
-            return tenderReportCount;
-        }
-    }
+	public List<TenderReportCount> findBySearchingKeyByDate(String searchKey) {
+		String sql = "SELECT * FROM TenderReport_Count_Wise";
+		return jdbcTemplate.query(sql, new TenderReportCountRowMapper());
+	}
 
-    public List<TenderReportCount> findByDepartmentAndOrganization(String searchKey,String searchValue) {
-    	String sql = "SELECT * FROM TenderReport_Count_Wise WHERE " + searchKey.toLowerCase() + " ILIKE ?";
-    	return jdbcTemplate.query(sql, new Object[]{"%" + searchValue.toLowerCase() + "%"}, new TenderReportCountRowMapper());
-    }
+	private static class TenderReportCountRowMapper implements RowMapper<TenderReportCount> {
 
-    public List<TenderReportCount> findByTenderId( String searchValue) {
-    	 String sql = "SELECT * FROM TenderReport_Count_Wise WHERE tenderid = ?";
-    	    return jdbcTemplate.query(sql, new Object[]{Double.parseDouble(searchValue)}, new TenderReportCountRowMapper());
-    	}
-    }
+		@Override
+		public TenderReportCount mapRow(ResultSet rs, int rowNum) throws SQLException {
+			TenderReportCount tenderReportCount = new TenderReportCount();
+
+			// Map columns of the ResultSet to fields of the TenderReportCount object
+			tenderReportCount.setSrNo(rs.getFloat("Sr_No"));
+			tenderReportCount.setTenderId(rs.getFloat("tenderid"));
+			tenderReportCount.setTenderNo(rs.getString("tenderNo"));
+			tenderReportCount.setTenderBrief(rs.getString("tenderBrief"));
+			tenderReportCount.setTotalEMDPaid(rs.getFloat("TotalEMDPaid"));
+			tenderReportCount.setTotalDocFees(rs.getFloat("TotalDocFees"));
+			tenderReportCount.setTotalEventwiseReg(rs.getFloat("TotalEventwiseReg"));
+			tenderReportCount.setTotalFinalSubmission(rs.getFloat("TotalFinalSubmission"));
+			tenderReportCount.setDomainName(rs.getString("domainName"));
+			tenderReportCount.setClientName(rs.getString("clientName"));
+			tenderReportCount.setStartDate(rs.getDate("StartDate").toLocalDate());
+			tenderReportCount.setEndDate(rs.getDate("EndDate").toLocalDate());
+			tenderReportCount.setPublishDate(rs.getDate("PublishDate").toLocalDate());
+			tenderReportCount.setOrganization(rs.getString("Organization"));
+			tenderReportCount.setDepartment(rs.getString("Department"));
+
+			return tenderReportCount;
+		}
+	}
+
+	public List<TenderReportCount> findByTenderId(String searchValue) {
+		String sql = "SELECT * FROM TenderReport_Count_Wise WHERE tenderid = ?";
+		return jdbcTemplate.query(sql, new Object[] { Double.parseDouble(searchValue) },
+				new TenderReportCountRowMapper());
+	}
+
+	public List<TenderReportCount> findByDepartmentAndOrganization(String searchKey, String searchValue) {
+		String sql = "SELECT * FROM TenderReport_Count_Wise WHERE " + searchKey.toLowerCase() + " ILIKE ?";
+		return jdbcTemplate.query(sql, new Object[] { "%" + searchValue.toLowerCase() + "%" },
+				new TenderReportCountRowMapper());
+	}
+	
+//	public List<TenderReportCount> findByDepartmentAndOrganization(String searchKey, String searchValue) {
+//		String sql = "SELECT * FROM TenderReport_Count_Wise WHERE Organization ILIKE ? OR Department ILIKE ?";
+//	    return jdbcTemplate.query(sql, new Object[] { "%" + searchValue.toLowerCase() + "%", "%" + searchValue.toLowerCase() + "%" },
+//	            new TenderReportCountRowMapper());
+//	}
+
+}
