b51f9d97ef91a6092c9e5f3d5b6374beba17533f
Vasik Shaikh <147395532+mdvasik@users.noreply.github.com>
2024-03-04 14:23:23 +0530

    Update TenderReportService.java
    final comment
    04/03/2024

--- Tender Fees Count Report/Tender-Fees-Count-Report/src/main/java/com/TenderReportCount/service/TenderReportService.java
+++ Tender Fees Count Report/Tender-Fees-Count-Report/src/main/java/com/TenderReportCount/service/TenderReportService.java
@@ -3,11 +3,11 @@ package com.TenderReportCount.service;
 import java.time.LocalDate;
 import java.util.List;
 
-import com.TenderReportCount.entity.TenderReportCount;
 import com.TenderReportCount.util.TenderReportResponse;
 
-public interface TenderReportService {
+public interface TenderService {
 
-	List<TenderReportResponse> tenderReportCount(String searchKey, LocalDate searchDate, String searchValue);
-
+    List<TenderReportResponse> getFilteredTenders(String departmentName, String domainName, LocalDate paymentDate,
+                                    LocalDate eventPublishDateFrom, LocalDate bidSubmissionDateFrom,
+                                    String eventStatus, Long eventId, String filterOperation);
 }
